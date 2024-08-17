5038c9fc17a4737aa7adc03d1911e5a72bd0e8bf
zafajardo9 <zafajardo9.programming@gmail.com>
2024-08-17 09:38:21 +0800

    keep

--- app/(public)/login/page.tsx
+++ app/(public)/login/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { LoginForm } from "@/components/module/user";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signIn = async (formData: FormData) => {
+  async function signIn(formData: FormData) {
     "use server";
 
     const email = formData.get("email") as string;
@@ -26,9 +26,9 @@ export default function Login({
     }
 
     return redirect("/notes");
-  };
+  }
 
-  const signUp = async (formData: FormData) => {
+  async function signUp(formData: FormData) {
     "use server";
 
     const origin = headers().get("origin");
@@ -49,7 +49,7 @@ export default function Login({
     }
 
     return redirect("/login?message=Check email to continue sign in process");
-  };
+  }
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
@@ -74,46 +74,11 @@ export default function Login({
         Back
       </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+      <LoginForm
+        signIn={signIn}
+        signUp={signUp}
+        message={searchParams?.message}
+      />
     </div>
   );
 }
